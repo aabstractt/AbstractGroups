adcdacc930a0ca6e014794ba7b7b1848e98517f0
wXav <xavier04.martinez@gmail.com>
2022-05-07 16:09:57 -0500

    action stuff

--- shared/src/main/java/dev/thatsmybaby/shared/actionlog/LoggedAction.java
+++ shared/src/main/java/dev/thatsmybaby/shared/actionlog/LoggedAction.java
@@ -1,5 +1,74 @@
 package dev.thatsmybaby.shared.actionlog;
 
+import dev.thatsmybaby.shared.sender.AbstractSender;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Instant;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE) @Getter
 public final class LoggedAction {
 
+    private final long timestamp;
+
+    private final String sourceXuid;
+    private final String sourceName;
+
+    private final String targetXuid;
+    private final String targetName;
+
+    public static @NonNull  Builder build() {
+        return new Builder();
+    }
+
+    public final static class Builder {
+
+        private long timestamp = 0L;
+
+        private String sourceXuid;
+        private String sourceName;
+
+        private String targetXuid;
+        private String targetName;
+
+        private Builder() {
+            // Not allow make instance
+        }
+
+        public @NonNull Builder timestamp(Instant instant) {
+            this.timestamp = instant.getEpochSecond();
+
+            return this;
+        }
+
+        public @NonNull Builder source(String sourceXuid, String sourceName) {
+            this.sourceXuid = sourceXuid;
+
+            this.sourceName = sourceName;
+
+            return this;
+        }
+
+        public @NonNull Builder source(AbstractSender sender) {
+            return this.source(sender.getXuid(), sender.getName());
+        }
+
+        public @NonNull Builder target(String targetXuid, String targetName) {
+            this.targetXuid = targetXuid;
+
+            this.targetName = targetName;
+
+            return this;
+        }
+
+        public @NonNull Builder target(AbstractSender sender) {
+            return this.source(sender.getXuid(), sender.getName());
+        }
+
+        public @NonNull LoggedAction build() {
+            return new LoggedAction(this.timestamp, this.sourceXuid, this.sourceName, this.targetXuid, this.targetName);
+        }
+    }
 }
--- shared/src/main/java/dev/thatsmybaby/shared/sender/AbstractSender.java
+++ shared/src/main/java/dev/thatsmybaby/shared/sender/AbstractSender.java
@@ -2,6 +2,10 @@ package dev.thatsmybaby.shared.sender;
 
 public abstract class AbstractSender {
 
+    public abstract String getName();
+
+    public abstract String getXuid();
+;
     public abstract boolean hasPermission(String permission);
 
     public abstract void sendMessage(String k, String... args);
