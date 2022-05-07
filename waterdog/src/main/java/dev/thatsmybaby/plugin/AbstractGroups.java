4ff44e3275925c4b57f1326fd4bda449f2202cab
wXav <xavier04.martinez@gmail.com>
2022-05-07 01:57:21 -0500

    waterdog support added

---
+++ waterdog/src/main/java/dev/thatsmybaby/plugin/AbstractGroups.java
@@ -0,0 +1,20 @@
+package dev.thatsmybaby.plugin;
+
+import dev.thatsmybaby.shared.VersionInfo;
+import dev.waterdog.waterdogpe.plugin.Plugin;
+import lombok.Getter;
+
+public final class AbstractGroups extends Plugin {
+
+    @Getter private static AbstractGroups instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        this.saveResource("config.yml");
+        this.saveResource("messages.yml");
+
+        VersionInfo.loadVersion();
+    }
+}
